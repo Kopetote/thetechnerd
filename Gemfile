a80678feb055fcba59b73708e70da1852b2f5af2
unknown <m.farrelazhar@gmail.com>
2024-06-16 17:35:25 +0700

    Ganti isi Welcome

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
-gem 'nokogiri'
+# gem 'racc'
+ gem 'nokogiri'
 gem 'rack', '~> 2.2.4'
 gem 'rspec'
 
@@ -9,4 +10,5 @@ gem "minimal-mistakes-jekyll"
 gem "jekyll-github-metadata"
 gem 'jekyll-archives'
 gem 'jekyll-seo-tag'
+
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Selamat Datang!",
-        "excerpt":"Halo Masyarakat Internet! Terima kasih udah dateng ke Github Page-ku. Di Page ini aku bakal sharing hal-hal teknis yang engga ada di Channel YouTube-ku. Di Pilot Episode Aku udah jelasin seri YouTube yang akan ku buat adalah Home Lab PC dan pada post selanjutnya aku akan memberikan bagaimana cara install...","categories": [],
+        "excerpt":"Halo Masyarakat Internet! Selamat datang di Github Page-ku. Github Pages ini akan menjadi blog untuk project-project engga ngotak / ngotak yang aku lakukan, seperti menggunakan iPhone IMEI ter block, menggunakan Arch Linux tanpa archinstall di laptop kerja, menggunakan komputer full TTY / CLI sebagai wind down computer, dan project lainnya...","categories": [],
         "tags": [],
         "url": "/Welcome/",
         "teaser": null
